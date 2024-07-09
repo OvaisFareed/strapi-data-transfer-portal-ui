3e512917d807915a59684182724badcadc1868eb
OvaisFareed <movaissiddiqui16@gmail.com>
2024-07-09 14:12:34 +0500

    update method to upload media

--- pages/api/strapi/upload.js
+++ pages/api/strapi/upload.js
@@ -3,36 +3,27 @@
 import { REMOTE_STRAPI_BASE_PATH } from "@/constants/environment";
 import { uploadAllMedia } from "@/services/api-service";
 import axios from "axios";
-import * as fs from 'fs';
-import * as filePath from 'path';
-import RunQueue from "run-queue";
-import * as Parallel from 'async-parallel';
 
 export default async function handler(req, res) {
     try {
         const { path } = req.query;
         const payload = req.body;
-        const formData = new FormData();
-        let count = 0;
-        let intervalID = '';
+        let formData = new FormData();
         let imageUrl = '';
         let file = {};
         let filesHash = {};
         let promises = [];
-        let promisesV2 = [];
         let missedFiles = [];
-        const avgUploadTimeInMS = 100;
         const itemsPerPage = 100;
         const totalItemsCount = payload.length;
         const noOfPages = Math.round((totalItemsCount) / itemsPerPage);
-        console.log('noOfPages: ', noOfPages);
 
         /*********************
         Promise.all approach
         **********************/
-        intervalID = setInterval(async () => {
-            console.log('range: ', count * itemsPerPage, (count * itemsPerPage) + itemsPerPage)
-            console.log('count: ', count);
+        for (let count = 0; count < noOfPages; count++) {
+            promises = [];
+            formData = new FormData();
             const images = payload.slice(count * itemsPerPage, (count * itemsPerPage) + itemsPerPage);
             for (let i = 0; i < images.length; i++) {
                 file = images[i];
@@ -51,15 +42,47 @@ export default async function handler(req, res) {
                 });
                 formData.append(`files`, blob, file.name);
             });
-            promises = [];
-            count++;
-            if (count === noOfPages) {
-                console.log('clearInterval');
-                clearInterval(intervalID);
+            if (count < noOfPages) {
+                console.log(`${(count * 100) + 100} files uploaded`);
                 await uploadAllMedia(`${path}`, formData);
             }
-        }, (totalItemsCount * avgUploadTimeInMS) / noOfPages);
+            if (count === noOfPages - 1) {
+                console.log('missedFiles count: ', missedFiles.length);
+                console.log('Job finished!');
+                break;
+            }
+        }
+
+        // Upload missed / rejected files
+        for (let count = 0; count < missedFiles.length; count++) {
+            promises = [];
+            formData = new FormData();
+            for (let i = 0; i < missedFiles.length; i++) {
+                file = missedFiles[i];
+                imageUrl = `${REMOTE_STRAPI_BASE_PATH}${file.url}`;
+                filesHash[imageUrl] = file;
+                promises.push(axios.get(imageUrl, { responseType: "arraybuffer" }));
+            }
+            const results = await Promise.allSettled(promises);
+            const files = results.filter(f => f.status === 'fulfilled');
+            files.forEach((res, index) => {
+                const file = filesHash[res?.value?.config?.url];
+                const blob = new Blob([res.value.data], {
+                    type: file.mime
+                });
+                formData.append(`files`, blob, file.name);
+            });
+            if (count < missedFiles.length) {
+                console.log(`${(count * 100) + 100} files uploaded`);
+                await uploadAllMedia(`${path}`, formData);
+            }
+            if (count === missedFiles.length - 1) {
+                console.log('missed files Job finished!');
+                break;
+            }
+        }
         res.status(200).json({ success: true, message: "All media uploaded successfully!" });
+
     } catch (error) {
         console.log('uploadAllMedia err: ', error)
         res.status(500).json({
@@ -75,7 +98,7 @@ export default async function handler(req, res) {
 export const config = {
     api: {
         bodyParser: {
-            sizeLimit: '10mb' // Set desired value here
+            sizeLimit: '500mb' // Set desired value here
         }
     }
 }
